543fa74238850bfc25044b5e06171292fed14864
Daniel McCarney <daniel@binaryparadox.net>
2014-05-20 11:04:15 -0700

    Adding line to bind debug server on non-localhost IP if required

--- run.py
+++ run.py
@@ -3,8 +3,16 @@
 # Only useful for testing. Deploy as a proper Apache mod_wsgi app
 # for anything more.
 #
-# Make sure you *never* run the app in Debug when publically accessible
-# as it allows arbitrary python code execution by design.
+# !!!! WARNING !!!! WARNING !!!! WARNING !!!! WARNING !!!! !!!! WARNING !!!!
+# Make sure you *never* run the app in Debug when accessible by untrusted
+# clients as it allows arbitrary python code execution by design.
+# !!!! WARNING !!!! WARNING !!!! WARNING !!!! WARNING !!!! !!!! WARNING !!!!
 #
 from observatory import app
+
+# To bind the debug instance on all interfaces (i.e. to access from another
+# machine) use the app.run line that specifies a host of 0.0.0.0
+#app.run(host='0.0.0.0', debug=True)
+
+# To bind the debug instance only to localhost, use the following app.run
 app.run(debug=True)
